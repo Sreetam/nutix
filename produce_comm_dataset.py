b6f9443747ae2886723f947d6c6b44fc216301f2
Sreetam <fearthesreetam@gmail.com>
2022-01-05 17:03:32 +0530

    create preprocessing code

---
+++ produce_comm_dataset.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from datetime import date, timedelta
+import numpy as np
+
+raw = pd.read_csv("peanuts.csv")
+pn = pd.DataFrame(columns=["price"], index=pd.to_datetime(raw.date), data=raw.price.values)
+raw = pd.read_csv("BrentOil.csv")
+bc = pd.DataFrame(columns=["price"], index=pd.to_datetime(raw.date), data=raw.price.values)
+bc = bc[pn.index[0]-timedelta(days=1):pn.index[-1]]
+bc.reset_index(inplace=True)
+pn.reset_index(inplace=True)
+
+oil = []
+peanut = []
+date = pd.date_range(bc.date[0],pn.date[359],freq='d')
+for i in date:
+  if i in list(pn.date):
+    peanut.append(pn.loc[pn["date"]==i].price.values[0])
+  else:
+    peanut.append(peanut[-1])
+  if i in list(bc.date):
+    oil.append(bc.loc[bc["date"]==i].price.values[0])
+  else:
+    oil.append(oil[-1])
+df = pd.DataFrame()
+df['peanut'] = peanut
+df['oil'] = oil
+df.index = date
+df.to_csv("commodities.csv")
